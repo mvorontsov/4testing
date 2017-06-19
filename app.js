7132393ad5e5e3031846895c9bb415643cfeced2
mvorontsov <mvorontsov@gmail.com>
2017-06-19 17:21:44 +0300

    usage express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('This is Node.js running on Android!')
+var express = require('express'),
+    app = express(),
+    port = Number(process.env.PORT || 8080);
+
+app.get('/', function(req, res) {
+   res.send('Express is working');
+});
+
+app.listen(port, function() {
+   console.log('Listening on port ' + port);
+});
